9e1d11b60d20381e1d6abac58e35e638fff1642c
kmts571011@Sai2 <mohanapriyanarapareddy@localhost>
2022-04-30 01:53:26 -0500

    length

---
+++ src/Strings/lastlength.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class lastlength {
+
+	public static void main(String[] args) {
+		String str= "we are good";
+	String[] strr=str.split(" ");
+	System.out.println(strr[strr.length-1].length());
+
+	}
+
+}
