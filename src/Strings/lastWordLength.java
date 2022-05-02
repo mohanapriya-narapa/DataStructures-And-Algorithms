96aabbaf578a70c0317e41bce8afe118e595f09f
kmts571011@Sai2 <mohanapriyanarapareddy@localhost>
2022-05-02 01:20:44 -0500

    5 problems

---
+++ src/Strings/lastWordLength.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class lastWordLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="Hello World";
+String[] s=str.split(" ");
+System.out.println(s[s.length-1].length());
+	}
+
+}
