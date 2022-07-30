c03d71de48f2b85efe014efaf178f767e877a094
mohanaTimeline <mohanapriyanarapareddy@localhost>
2022-07-30 15:01:23 -0500

    Leetcode Arrays

---
+++ src/Leetcode/BuildArrayfromPermutation.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+public class BuildArrayfromPermutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] arr=new int[] {0,2,1,5,3,4};
+int[] res=buildArray(arr);
+for(int x:res)
+{
+	System.out.println(x);
+}
+	}
+
+	private static int[] buildArray(int[] arr) {
+		// TODO Auto-generated method stub
+		int[] res=new int[arr.length];
+		for(int i=0;i<arr.length;i++)
+		{
+			res[i]=arr[arr[i]];
+		}return res;
+	}
+
+}
---
+++ src/Leetcode/ConcatenationOfArray.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+public class ConcatenationOfArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] nums=new int[]  {1,2,1};
+int[] res= ConcatenationOfArray(nums);
+for(int x:res)
+{
+	System.out.println(x);
+}
+ 	}
+
+	private static int[] ConcatenationOfArray(int[] nums) {
+		// TODO Auto-generated method stub
+	int[] res=new int[nums.length*2];
+	for(int i=0;i<nums.length;i++)
+	{
+		res[i]=nums[i];
+		res[i+nums.length]=nums[i];
+	}return res;
+	}
+
+}
