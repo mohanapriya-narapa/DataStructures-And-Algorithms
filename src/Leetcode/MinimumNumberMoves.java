1ca7cb7209a90d4d2fed04a7e43485eb8a27b729
kmts571011@Sai2 <mohanapriyanarapareddy@localhost>
2022-07-31 15:24:55 -0500

    leetcode

---
+++ src/Leetcode/MinimumNumberMoves.java
@@ -0,0 +1,27 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class MinimumNumberMoves {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] seats=new int[] {3,1,5};
+int[] students=new int[] {2,7,4};
+System.out.println(minMovesToSeat(seats, students));
+
+	}
+
+	private static int minMovesToSeat(int[] seats, int[] students) {
+		// TODO Auto-generated method stub
+		Arrays.sort(seats);
+        Arrays.sort(students);
+        int ans=0;
+        for(int i=0;i<seats.length;i++)
+        {
+            ans+=Math.abs(seats[i]-students[i]);
+        }
+        return ans;
+	}
+
+}
---
+++ src/Leetcode/countKDifference.java
@@ -0,0 +1,28 @@
+package Leetcode;
+
+public class countKDifference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] nums=new int[] {1,2,2,1};
+int k=1;
+System.out.println(countKDifference(nums,k));
+	}
+
+	private static int countKDifference(int[] nums, int k) {
+		// TODO Auto-generated method stub
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(Math.abs(nums[i]-nums[j])==k)
+                {
+                    count++;
+                }
+            }
+        }
+       return count;
+	}
+
+}
