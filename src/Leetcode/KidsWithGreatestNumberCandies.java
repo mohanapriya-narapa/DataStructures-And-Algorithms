c1e70377c446a108d5edf9d43a781fe55d03bb23
mohanaTimeline <mohanapriyanarapareddy@localhost>
2022-07-30 20:42:33 -0500

    leetcode

---
+++ src/Leetcode/KidsWithGreatestNumberCandies.java
@@ -0,0 +1,34 @@
+package Leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class KidsWithGreatestNumberCandies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] candies=new int[] {2,3,5,1,3};
+System.out.println(kidsWithCandies(candies, 3));
+	}
+
+	private static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+		// TODO Auto-generated method stub
+	    Boolean[] result=new Boolean[candies.length];
+	    Arrays.fill(result, Boolean.FALSE);
+	      int max=0;
+	           for(int x: candies)
+	           {
+	               max=Math.max(max,x);
+	           }
+	           for(int i=0;i<candies.length;i++)
+	           {
+	               if((candies[i]+extraCandies)>=max)
+	               {
+	                   result[i]=true;
+	               }
+	           }
+	           return  Arrays.asList(result);
+	}
+
+}
+ 
---
+++ src/Leetcode/NumbersSmallerThanCurrentNumber.java
@@ -0,0 +1,33 @@
+package Leetcode;
+
+public class NumbersSmallerThanCurrentNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums=new int[] {8,1,2,2,3};
+int[] res=smallerNumbersThanCurrent(nums);
+for(int x:res)
+{
+	System.out.println(x);
+}
+	}
+
+	private static int[] smallerNumbersThanCurrent(int[] nums) {
+		// TODO Auto-generated method stub
+		int[] res=new int[nums.length];
+		for(int i=0;i<nums.length;i++)
+		{
+			int count=0;
+			for(int j=0;j<nums.length;j++)
+			{
+				if(nums[i]>nums[j])
+				{
+					count++;
+				}
+			}
+			res[i]=count;
+		}
+		return res;
+	}
+
+}
