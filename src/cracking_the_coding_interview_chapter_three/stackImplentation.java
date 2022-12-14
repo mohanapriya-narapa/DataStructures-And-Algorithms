1e5515fba6538dd837e267277cb6e3f87a7b5e0d
kmts571011@Sai2 <mohanapriyanarapareddy@localhost>
2022-12-14 15:42:48 -0600

    stack implementation

--- src/cracking_the_coding_interview_chapter_three/stackImplentation.java
+++ src/cracking_the_coding_interview_chapter_three/stackImplentation.java
@@ -2,16 +2,20 @@ package cracking_the_coding_interview_chapter_three;
 
 public class stackImplentation {
 	public static int[] heads=new int[3];
-	static int stacknum=3;static int stackSize=3;
+	static int stacknum=2;static int stackSize=2;
 	static int[] arr=new int[stacknum*stackSize];
 	public static void main(String[] args) {
 		for(int i=0;i<arr.length;i++)
 		{
 			arr[i]=-1;
 		}
+		for(int i=0;i<heads.length;i++)
+		{
+			heads[i]=-1;
+		}
 		// TODO Auto-generated method stub
-push(5,0);push(8,1);System.out.println(pop(1));push(9,2);push(10,2);System.out.println(peek(2));System.out.println(empty(1));
-
+push(5,0);push(6,0);push(8,1);push(9,1);
+		System.out.println(peek(1));System.out.println(empty(1));
 	}
 
 	private static boolean empty(int stacknum) {
@@ -26,8 +30,7 @@ push(5,0);push(8,1);System.out.println(pop(1));push(9,2);push(10,2);System.out.p
 		// TODO Auto-generated method stub
 		heads[stacknum]++;
 		int offset=arrayOffset(stacknum)+heads[stacknum];
-		arr[offset]=i;
-		
+		arr[offset]=i;	
 	}
 
 	private static int arrayOffset(int stacknum) {
@@ -42,13 +45,11 @@ push(5,0);push(8,1);System.out.println(pop(1));push(9,2);push(10,2);System.out.p
 		}
 		int offset=arrayOffset(stacknum)+heads[stacknum];
 		heads[stacknum]--;
-		return arr[offset];
-		
+		return arr[offset];		
 	}
 	private static int peek(int stacknum) {
 		// TODO Auto-generated method stub
 		int offset=arrayOffset(stacknum)+heads[stacknum];
-		return arr[offset];
-		
+		return arr[offset];	
 	}
 }
